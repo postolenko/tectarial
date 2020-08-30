d6314d07ab458626d9d3e52a6c7045746c55d331
postolenko <denis.postolenko@gmail.com>
2020-08-30 22:01:44 +0300

    Tectarial

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/tectarial/index.html
---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+$(window).load(function() {
+
+
+
+});
+
+$(window).resize(function() {
+
+
+
+});
+
+$(document).scroll(function() {
+
+
+
+});
+
+$(document).ready(function() {
+
+    if( $(".main_sldier").length > 0 ) {
+        $(".main_sldier").not(".slick-initialized").slick({
+            dots: false,
+            arrows: true,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true
+        });
+    }
+
+});
